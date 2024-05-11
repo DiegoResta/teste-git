dba1946f9e841c7f5b239431ef6362a292698c4f
Diego Resta <diegorestajc@gmail.com>
2024-05-11 14:10:21 -0300

    commit 1

---
+++ static/issue-panel/src/App.js
@@ -0,0 +1,77 @@
+import React, { Fragment, useCallback, useState } from 'react';
+
+import Button from '@atlaskit/button';
+import { Field, HelperMessage } from '@atlaskit/form';
+import Textfield from '@atlaskit/textfield';
+
+import Modal, {
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  ModalTitle,
+  ModalTransition,
+} from '@atlaskit/modal-dialog';
+
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [name, setName] = useState('');
+
+  const openModal = useCallback(() => setIsOpen(true), []);
+  const closeModal = useCallback(() => setIsOpen(false), []);
+
+  const onSubmit = () => {
+    
+      e.preventDefault();      
+    
+    setName("")
+  };
+  
+   
+  return (
+    <div>
+      <Button appearance="primary" onClick={openModal}>
+        Open modal
+      </Button>
+
+      <ModalTransition>
+        {isOpen && (
+          <Modal onClose={closeModal}>
+            <form onSubmit={onSubmit}>
+              <ModalHeader>
+                <ModalTitle>Create a user</ModalTitle>
+              </ModalHeader>
+              <ModalBody>
+                <Field id="name" name="name" label="Type your name to continue">
+                  {({ fieldProps }) => (
+                    <Fragment>
+                      <Textfield
+                        {...fieldProps}
+                        defaultValue="Ian Atlas"
+                        value={undefined}
+                      />
+                      <HelperMessage>
+                        {name ? `Hello, ${name}` : ''}
+                      </HelperMessage>
+                    </Fragment>
+                  )}
+                </Field>
+              </ModalBody>
+              <ModalFooter>
+                <Button appearance="subtle" onClick={closeModal}>
+                  Close
+                </Button>
+                <Button appearance="primary" type="submit">
+                  Create
+                </Button>
+              </ModalFooter>
+            </form>
+          </Modal>
+        )}
+      </ModalTransition>
+         
+    </div>
+  );
+}
+
+export default App;
